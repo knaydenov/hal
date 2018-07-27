db4c9efa380136b6bf695d5b2e0fa20d56cedcd8
Klim Naydenov <knaydenov@gmail.com>
2018-07-27 17:49:37 +0300

    New implementation

--- lib/http-service.ts
+++ lib/http-service.ts
@@ -2,8 +2,8 @@ import { Observable } from 'rxjs/Observable';
 import { IResource } from './resource';
 
 export interface IHttpService {
-    get: <I extends IResource>(url: string, options?: any) => Observable<I | null>;
-    post: <I extends IResource>(url: string, data: any, options?: any) => Observable<I | null>;
-    patch: <I extends IResource>(url: string, data: any, options?: any) => Observable<I | null>;
-    delete: <I extends IResource>(url: string, options?: any) => Observable<I | null>;
+    get: <I extends IResource>(url: string, options?: any) => Observable<I>;
+    post: <I extends IResource>(url: string, data: any, options?: any) => Observable<I>;
+    patch: <I extends IResource>(url: string, data: any, options?: any) => Observable<I>;
+    delete: <I extends IResource>(url: string, options?: any) => Observable<null>;
 };
--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
+export * from './hal';
 export * from './resource';
+export * from './storage';
 export * from './collection-resource';
 export * from './pageable-resource';
 export * from './http-service';
